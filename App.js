b974ffb2aaeeac0b942fb475ef5000c00e583154
ptchblckchocopie <161784941+ptchblckchocopie@users.noreply.github.com>
2024-11-06 16:12:52 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import User from "./screens/User";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
+    </NavigationContainer> 
+  );
+}
